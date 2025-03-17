1e67ab1251301d9c426b8fd7f9b97051fb1658b8
miku116 <qingyuanhuang@foxmail.com>
2025-03-17 16:42:24 +0800

    完善snake身体生长机制，建立游戏结束机制，碰墙、碰撞身体结束游戏，修复Bug：当只有蛇头时可以自由移动，有身体时无法移动向相反方向，解决回头碰撞自己身体导致游戏结束，完善apple出现区域

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -6,6 +6,10 @@ public class Snake : MonoBehaviour
 {
     Vector3 direction;
     public float speed;
+    //蛇身节点预制体
+    public Transform bodyPrefab;
+    //用List来存储蛇身的各个节点
+    public List<Transform> bodies = new List<Transform>();
 
     // Start is called before the first frame update
     void Start()
@@ -13,6 +17,8 @@ public class Snake : MonoBehaviour
         //Debug.Log(transform.position);
 
         Time.timeScale = speed; //0.3倍速
+        //初始化蛇头
+        Resetstage();
 
 
     }
@@ -20,30 +26,79 @@ public class Snake : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 当前移动方向的反方向
+        Vector3 forbiddenDirection = Vector3.zero;
+        if (bodies.Count > 1)
+        {
+            forbiddenDirection = -direction; // 计算当前方向的相反方向
+        }
         if (Input.GetKeyDown(KeyCode.W))
         {
             //Debug.Log("W");
-            direction = Vector3.up;
+            if (bodies.Count == 1 || forbiddenDirection != Vector3.up)
+                direction = Vector3.up;
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             //Debug.Log("S");
-            direction = Vector3.down;
+            if (bodies.Count == 1 || forbiddenDirection != Vector3.down)
+                direction = Vector3.down;
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             //Debug.Log("A");
-            direction = Vector3.left;
+            if (bodies.Count == 1 || forbiddenDirection != Vector3.left)
+                direction = Vector3.left;
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             //Debug.Log("D");
-            direction = Vector3.right;
+            if (bodies.Count == 1 || forbiddenDirection != Vector3.right)
+                direction = Vector3.right;
         }
     }
     //FixedUpdate 每秒更新50次
     private void FixedUpdate()
     {
-        transform.Translate(direction * speed);
+        for(int i = bodies.Count-1; i > 0; i--)
+        {
+            bodies[i].position = bodies[i-1].position;
+        }
+
+        transform.Translate(direction);
+    }
+
+    //碰撞检测，当角色与其他物体（tag为"Food"的物体）发生碰撞时触发
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food"))
+        {
+            //Instantiate(bodyPrefab);
+            bodies.Add(Instantiate(bodyPrefab, transform.position, 
+                                   Quaternion.identity));
+        }
+        //当角色与墙壁发生碰撞时触发
+        if (collision.CompareTag("Obstacle"))
+        {
+            //Debug.Log("Game Over");
+            Resetstage();
+        }
+        
+    }
+    
+    void Resetstage()
+        {
+        direction = Vector3.zero;
+        transform.position = Vector3.zero;
+
+        //删除蛇身节点,但保留蛇头
+        for (int i = 1; i < bodies.Count; i++)
+        {
+            Destroy(bodies[i].gameObject);
+        }
+        //清空蛇身列表
+        bodies.Clear();
+        //再次存储添加蛇头
+        bodies.Add(transform);
     }
 }
